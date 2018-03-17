1214522ab3b06f9d79bc17bca0cdbf2399883a66
lornlorn <dl_lorn@aliyun.com>
2018-03-17 22:34:15 +0800

    update20180317

--- app/httpsvr/handler/index.go
+++ app/httpsvr/handler/index.go
@@ -1,11 +1,19 @@
 package handler
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 // IndexHandle func(res http.ResponseWriter, req *http.Request)
 func IndexHandle(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "Route Index : %v\n", req.URL)
+	log.Printf("Route Index : %v\n", req.URL)
+	tmpl, err := template.ParseFiles("views/index.html")
+	if err != nil {
+		log.Printf("Parse Error : %v\n", err)
+		return
+	}
+
+	tmpl.Execute(res, nil)
 }
